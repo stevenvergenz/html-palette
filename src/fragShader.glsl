d4d9d888940bb06f69c3f59956018f242cae8dbc
Steven Vergenz <vergenzs@gmail.com>
2015-09-16 16:52:53 -0400

    Removed redundant code

--- src/fragShader.glsl
+++ src/fragShader.glsl
@@ -10,13 +10,6 @@ uniform bool radial;
 uniform bool useAlpha;
 uniform sampler2D tex;
 
-vec3 hsv2rgb(float h, float s, float v){
-	vec3 c = vec3(h,s,v);
-	vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-	vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-	return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-
 vec3 hsv2rgb(vec3 c){
 	vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
 	vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
